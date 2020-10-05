2af4e134ecc34956bea27580c06ca69c2350ae0b
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-10-05 15:19:25 -0400

    created server and git ignore

---
+++ controllers/controller.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    
+      
+      res.render("index")
+    
+  });
+
+  // Export routes for server.js to use.
+module.exports = router;
